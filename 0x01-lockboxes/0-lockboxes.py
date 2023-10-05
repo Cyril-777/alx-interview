ea21c46368149e437bebac76dee875e895664b36
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-10-05 03:53:40 +0300

    another try

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,16 +10,14 @@ def canUnlockAll(boxes):
     """
     A method that determines if all the boxes can be opened
     """
-    n = len(boxes)  # Number of boxes
-    opened = [False] * n  # Initialize all boxes as unopened
-    opened[0] = True
-
     stack = [0]
-    while stack:
-        currBox = stack.pop()
-        for key in boxes[currBox]:
-            if not opened[key]:
-                opened[key] = True
-                stack.append(key)
 
-    return all(opened)
+    for id, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in stack and key != id:
+                stack.append(key)
+    if len(stack) == len(boxes):
+        return True
+    return False
