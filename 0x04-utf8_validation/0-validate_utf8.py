2a08c5086eb44248bee139f51d3378b6c64731bb
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-10-26 18:12:01 +0300

    less lines

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-A method that determines if a given data set represents a valid UTF-8 encoding
+"""A method to see if a given data set represents a valid UTF-8 encoding
 """
 
 
@@ -25,6 +24,5 @@ def validUTF8(data):
                 return False
 
             num_bytes -= 1
-
     # If there are remaining bytes, it's an incomplete character
     return num_bytes == 0
