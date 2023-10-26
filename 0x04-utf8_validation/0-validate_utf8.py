836377e7fef9895dc29900d9ca0c0b8668adaf25
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-10-26 18:26:52 +0300

    solution #2

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,21 +8,23 @@ def validUTF8(data):
     num_bytes = 0
 
     for byte in data:
-        # Check if the byte is a valid UTF-8 start byte
+        # Ensure we're working with 8 bits
+        byte = byte & 0xFF
+
         if num_bytes == 0:
-            if (byte >> 5) == 0b110:
+            if (byte >> 7) == 0:
+                continue
+            elif (byte >> 5) == 0b110:
                 num_bytes = 1
             elif (byte >> 4) == 0b1110:
                 num_bytes = 2
             elif (byte >> 3) == 0b11110:
                 num_bytes = 3
-            elif (byte >> 7):
-                return False  # Invalid start byte
+            else:
+                return False
         else:
-            # Check if the byte is a valid UTF-8 continuation byte
             if (byte >> 6) != 0b10:
                 return False
-
             num_bytes -= 1
-    # If there are remaining bytes, it's an incomplete character
+
     return num_bytes == 0
